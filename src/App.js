abae181f03698cbba42689fd0889b398128dafa3
CJ Rasos <cj.rasos30@gmail.com>
2024-10-28 18:59:17 -0700

    Add all components for site

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
     </div>
   );
 }
