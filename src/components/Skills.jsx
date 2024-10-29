5385e39601a359f8b967d50b744b907974718558
CJ Rasos <cj.rasos30@gmail.com>
2024-10-29 11:20:48 -0700

    Update skills by adding hyperlinks for each language

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
 import python from "../assets/python.png";
 import github from "../assets/github.png";
-import cpp from "../assets/cpp.png";
+import cpp from "../assets/cpp.png"; 
 
 const Skills = () => {
   const langs = [
@@ -13,31 +13,37 @@ const Skills = () => {
       id: 1,
       src: html,
       title: "HTML",
- },
+      link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    },
     {
       id: 2,
       src: css,
       title: "CSS",
+      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     },
     {
       id: 3,
       src: javascript,
       title: "JavaScript",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
       id: 4,
       src: python,
       title: "Python",
+      link: "https://docs.python.org/3/",
     },
     {
       id: 5,
       src: github,
       title: "GitHub",
+      link: "https://docs.github.com/en",
     },
     {
       id: 6,
       src: cpp,
       title: "C++",
+      link: "https://en.cppreference.com/w/",
     },
   ];
 
@@ -46,7 +52,7 @@ const Skills = () => {
       name="Skills"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col items-center justify-center w-full h-full text-white">
         <div>
           <p className="text-3xl font-bold inline border-b-4 border-[#de143b]">
             Skills
@@ -54,14 +60,19 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {langs.map(({ id, src, title}) => (
-            <div
+          {langs.map(({ id, src, title, link }) => (
+            <a
               key={id}
-              className={`px-4 cursor-pointer capitalize font-medium text-white-500 hover:scale-150 duration-200`}
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+              className="px-4 cursor-pointer transform hover:scale-110 duration-200 text-white hover:text-[#de143b]" 
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
-            </div>
+              <div>
+                <img src={src} alt={title} className="w-20 mx-auto" />
+                <p className="mt-4">{title}</p>
+              </div>
+            </a>
           ))}
         </div>
       </div>
